a9661e521a945d2772c54ed1e465396052fbe71b
Sharad Bhat <sharad.mbhat@gmail.com>
2018-06-29 03:11:22 +0530

    Added readable.js

---
+++ readable.js
@@ -0,0 +1,83 @@
+const fs = require("fs");
+
+/**
+ * Initializes the object.
+ * @param {boolean} capitalize - If set to true, returns string in CamelCase.
+ */
+function readable(capitalize=true, wordCount=3) {
+  if (wordCount < 2) {
+    throw new Error("Minimum value expected: 2");
+  }
+  else if (wordCount > 10) {
+    throw new Error("Maximum value expected: 10");
+  }
+
+  this.capitalize = capitalize;
+  this.wordCount = wordCount;
+
+  this.vowels = ["a", "e", "i", "o", "u"];
+  this.adjectives = fs.readFileSync(__dirname + "\\words\\adjectives.txt").toString().split(" ");
+  this.nouns = fs.readFileSync(__dirname + "\\words\\nouns.txt").toString().split(" ");
+}
+
+/**
+ * Converts each word in list to title case.
+ * @param {string[]} wordsList - The array of words to be capitalized.
+ * @returns {string[]} - The array with each word capitalized.
+ */
+readable.prototype.convertToTitleCase = function (wordsList) {
+  for (var i = 0; i < wordsList.length; i++) {
+    wordsList[i] = wordsList[i].charAt(0).toUpperCase() + wordsList[i].slice(1).toLowerCase();
+  }
+  return wordsList;
+}
+
+/**
+ * Generates the string.
+ * @returns {string} - The randomly generated string.
+ */
+readable.prototype.generate = function () {
+  wordsList = [];
+  wordsList.push(this.adjectives[Math.floor(Math.random() * this.adjectives.length)]);
+  wordsList.push(this.nouns[Math.floor(Math.random() * this.nouns.length)]);
+
+  if (this.wordCount > 5){
+    for (var i = 0; i < this.wordCount - 2; i++) {
+      wordsList.unshift(this.adjectives[Math.floor(Math.random() * this.adjectives.length)]);
+    }
+  }
+  else {
+    if (this.wordCount > 2) {
+      wordsList.unshift(this.adjectives[Math.floor(Math.random() * this.adjectives.length)]);
+    }
+
+    if (this.wordCount > 3) {
+      var isVowel = false;
+      var firstLetter = wordsList[0][0];
+      for(var i = 0; i < 5; i++)
+      {
+        if (this.vowels[i] === firstLetter) {
+          isVowel = true;
+          break;
+        }
+      }
+      if (isVowel) {
+        wordsList.unshift("an");
+      }
+      else {
+        wordsList.unshift(["a", "the"][Math.floor(Math.random() * 2)]);
+      }
+    }
+
+    if (this.wordCount > 4) {
+      wordsList.splice(2, 0, "and");
+    }
+  }
+
+  if (this.capitalize) {
+    wordsList = this.convertToTitleCase(wordsList);
+  }
+  return wordsList.join("");
+}
+
+module.exports = readable;
